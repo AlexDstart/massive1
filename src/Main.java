9dbdb7210fb9c5e9ddf9976860f0135d08253e5b
AlexDstart <demyanalex@gmail.com>
2022-12-05 21:41:29 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int[]arr1=new int[3];
+        arr1[0]=1;
+        arr1[1]=2;
+        arr1[2]=3;
+        double[]arr2={1.57,7.654,9.986};
+        char[]arr3={'a','b','c','d','e'};
+
+    }
+}
