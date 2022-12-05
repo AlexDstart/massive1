bf8de46c7024d4461c2f9028f200915765680318
AlexDstart <demyanalex@gmail.com>
2022-12-05 21:52:40 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,29 @@ public class Main {
         arr1[2]=3;
         double[]arr2={1.57,7.654,9.986};
         char[]arr3={'a','b','c','d','e'};
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+            if (i != arr1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+                System.out.println();
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i != arr2.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i]);
+            if (i != arr3.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+            }
 
-    }
-}
+        }
+
+
